e4ee23677d245e1fd51d2839f2a06c8e8a3464a4
Harsh Singh <sengarharshsinghrajput@gmail.com>
2025-01-04 10:46:20 +0530

    final commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const inputBox = document.querySelector('.input-box');
-const searchBtn = document.querySelector('#searchBtn');
+const searchBtn = document.getElementById('searchBtn');
 const weather_img = document.querySelector('.weather-img');
 const temperature = document.querySelector('.temperature');
 const description = document.querySelector('.description');
-const humidity = document.querySelector('#humidity');
-const wind_speed = document.querySelector('#wind-speed');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
 
 const location_not_found = document.querySelector('.location-not-found');
 
