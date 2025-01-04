7bd74f4c1c41e8107be3f2cb125bef531c281af6
Harsh Singh <sengarharshsinghrajput@gmail.com>
2025-01-04 10:35:27 +0530

    selectors added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const inputBox = document.querySelector('.input-box');
-const searchBtn = document.getElementById('searchBtn');
+const searchBtn = document.querySelector('#searchBtn');
 const weather_img = document.querySelector('.weather-img');
 const temperature = document.querySelector('.temperature');
 const description = document.querySelector('.description');
-const humidity = document.getElementById('humidity');
-const wind_speed = document.getElementById('wind-speed');
+const humidity = document.querySelector('#humidity');
+const wind_speed = document.querySelector('#wind-speed');
 
 const location_not_found = document.querySelector('.location-not-found');
 
